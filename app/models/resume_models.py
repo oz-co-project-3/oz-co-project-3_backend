b78c8497a1bbce63a15e03c3826a802c95659cbe
Ibiella <rubyhae@gmail.com>
2025-04-08 18:33:42 +0900

    :sparkles: Feat: 이력서 기본 모델 작성

--- app/models/resume_models.py
+++ app/models/resume_models.py
@@ -10,31 +10,34 @@ class StatusEnum(str, Enum):
     Closed = "완료"
 
 
-class WorkExp(Model):
-    id = fields.IntField(pk=True)
-    user = fields.ForeignKeyField(Users.model, related_name="experience")
-    company = fields.CharField(max_length=255)
-    period = fields.CharField(max_length=255)
-    position = fields.CharField(max_length=255)
-
-
 class Resume(Model):
     id = fields.IntField(pk=True)
-    user = fields.ForeignKeyField(Users.models, related_name="writer")
+    user = fields.ForeignKeyField(
+        Users.models, related_name="resumes", on_delete=fields.CASCADE
+    )
     title = fields.CharField(max_length=255)
     visibility = fields.BooleanField(default=True)
     name = fields.CharField(max_length=255)
     phone_number = fields.CharField(max_length=255)
     email = fields.CharField(max_length=255)
-    image_profile = fields.CharField(max_length=255, blank=True)
+    image_profile = fields.CharField(max_length=255, null=True, default="")
     interests = fields.JSONField(default=[])
     desired_area = fields.CharField(max_length=255)
-    work_exp = fields.ForeignKeyField(WorkExp.model, related_name="experience")
-    education = fields.CharField(max_length=255, blank=True)
-    school_name = fields.CharField(max_length=255, blank=True)
-    graduation_status = fields.CharField(max_length=255, blank=True)
+    education = fields.CharField(max_length=255, null=True)
+    school_name = fields.CharField(max_length=255, null=True)
+    graduation_status = fields.CharField(max_length=255, null=True)
     introduce = fields.TextField()
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
     status = fields.CharEnumField(StatusEnum, default=StatusEnum.Writing)
-    document_url = fields.CharField(max_length=255, blank=True)
+    document_url = fields.CharField(max_length=255, null=True)
+
+
+class WorkExp(Model):
+    id = fields.IntField(pk=True)
+    resume = fields.ForeignKeyField(
+        Resume.model, related_name="work_experiences", on_delete=fields.CASCADE
+    )
+    company = fields.CharField(max_length=255)
+    period = fields.CharField(max_length=255)
+    position = fields.CharField(max_length=255)
