7fa2b4fef502ce91bb23b30c300ed052d093ae5d
Ibiella <rubyhae@gmail.com>
2025-04-08 18:04:56 +0900

    :sparkles: Feat: 경력사항 모델 작성

--- app/resume/models.py
+++ app/resume/models.py
@@ -10,23 +10,31 @@ class StatusEnum(str, Enum):
     Closed = "완료"
 
 
+class WorkExp(Model):
+    id = fields.IntField(pk=True)
+    user = fields.ForeignKeyField(Users.model, related_name="experience")
+    company = fields.CharField(max_length=255)
+    period = fields.CharField(max_length=255)
+    position = fields.CharField(max_length=255)
+
+
 class Resume(Model):
     id = fields.IntField(pk=True)
-    user = fields.ForeignKeyField("users.models", related_name="writer")
+    user = fields.ForeignKeyField(Users.models, related_name="writer")
     title = fields.CharField(max_length=255)
     visibility = fields.BooleanField(default=True)
     name = fields.CharField(max_length=255)
     phone_number = fields.CharField(max_length=255)
     email = fields.CharField(max_length=255)
-    image_profile = fields.CharField(max_length=255, null=True, blank=True)
+    image_profile = fields.CharField(max_length=255, blank=True)
     interests = fields.JSONField(default=[])
     desired_area = fields.CharField(max_length=255)
-    work_exp = fields.ForeignKeyField("work.exp.models", related_name="experience")
-    education = fields.CharField(max_length=255, null=True, blank=True)
-    school_name = fields.CharField(max_length=255, null=True, blank=True)
-    graduation_status = fields.CharField(max_length=255, null=True, blank=True)
+    work_exp = fields.ForeignKeyField(WorkExp.model, related_name="experience")
+    education = fields.CharField(max_length=255, blank=True)
+    school_name = fields.CharField(max_length=255, blank=True)
+    graduation_status = fields.CharField(max_length=255, blank=True)
     introduce = fields.TextField()
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
     status = fields.CharEnumField(StatusEnum, default=StatusEnum.Writing)
-    document_url = fields.CharField(max_length=255, null=True, blank=True)
+    document_url = fields.CharField(max_length=255, blank=True)
