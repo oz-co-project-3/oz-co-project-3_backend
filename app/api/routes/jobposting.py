e9e4b4831ea1e5adcbd5e2754fd84e1f888597cb
Ibiella <rubyhae@gmail.com>
2025-04-09 17:17:57 +0900

    :sparkles: Feat: exception 세팅

--- app/api/routes/jobposting.py
+++ app/api/routes/jobposting.py
@@ -1,1 +1,3 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+
+from app.schemas.jobposting_schemas import JobPostingCreateUpdate, JobPostingResponse
--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from tortoise.contrib.fastapi import register_tortoise
+from utils.exception import CustomException
 
 from app.core.config import TORTOISE_ORM
 
@@ -17,3 +19,16 @@ register_tortoise(
     generate_schemas=False,
     add_exception_handlers=True,
 )
+
+
+@app.exception_handler(CustomException)
+async def custom_exception_handler(request: Request, exc: CustomException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "message": {
+                "error": exc.error,
+                "code": exc.code,
+            }
+        },
+    )
