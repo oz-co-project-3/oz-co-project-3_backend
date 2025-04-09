c2a58c71c4dd14645560d617e96160d09b83d9ec
2joonkim <joon271694@gmail.com>
2025-04-09 13:16:29 +0900

    ✨Feat: user-model 작성

--- app/core/config.py
+++ app/core/config.py
@@ -23,8 +23,13 @@ TORTOISE_ORM = {
     "apps": {
         "models": {
             "models": [
-                "aerich.models",
+                "app.models.success_review_models",
+                "app.models.free_board_models",
+                "app.models.resume_models",
+                "app.models.comment_models",
+                "app.models.job_posting_models",
                 "app.models.user_models",
+                "aerich.models",
             ],
             "default_connection": "default",
         }
