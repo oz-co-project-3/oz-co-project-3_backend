df0c72a7b21ce2ea1c0e069fc544c45fed1f791f
hak2881 <gkr054@naver.com>
2025-04-12 23:21:47 +0900

    ✨Feat: comment API 생성

--- app/services/freeboard_services.py
+++ app/services/freeboard_services.py
@@ -18,7 +18,7 @@ def existing_board(board):
 
 def author_board(board, user):
     """작성자인지 확인하는 함수"""
-    if board.user != user and not board.user.is_superuser:
+    if board.user != user and not user.is_superuser:
         raise CustomException(
             error="작성자가 아닙니다.", code="permission_denied", status_code=403
         )
