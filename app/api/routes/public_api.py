dec3dc52242bcbbae8c6a790055b42e07b5dda09
Ibiella <rubyhae@gmail.com>
2025-04-21 02:47:19 +0900

    :sparkles: Feat: 공공 api 가져오기, 저장 및 불러오기 router

--- app/api/routes/public_api.py
+++ app/api/routes/public_api.py
@@ -2,6 +2,9 @@ import os
 
 from fastapi import APIRouter
 
+from app.models.job_posting_models import JobPosting
+from app.schemas.jobposting_schemas import JobPostingResponse
+from app.services.public_collection import save_job_postings
 from app.services.public_services import ExternalAPIService
 from app.utils.exception import CustomException
 
@@ -17,14 +20,26 @@ api_service = ExternalAPIService(
 )
 
 
-@router.get("/fetch-data")
-async def fetch_data():
+# 외부 데이터 가져오기 및 저장
+@router.post("/update-job-postings")
+async def update_job_postings():
     try:
-        endpoint = "/Oldpsnslfjobbiz"
-        params = {"query": "example"}
-        response = await api_service.get_data(endpoint=endpoint, params=params)
-        return response
-    except CustomException as e:
-        return {
-            "error": str(e),
-        }
+        await save_job_postings()  # 데이터 저장 로직 호출
+        return {"message": "Job postings updated successfully"}
+    except Exception as e:
+        raise CustomException(
+            error="등록에 실패하였습니다.", code="update_failure", status_code=500
+        )
+
+
+@router.get("/job-postings", response_model=list[JobPostingResponse])
+async def get_job_postings():
+    try:
+        job_postings = await JobPosting.all()
+        return [
+            JobPostingResponse(**job_posting).dict() for job_posting in job_postings
+        ]
+    except Exception as e:
+        raise CustomException(
+            error="불러오기에 실패했습니다", code="fetch_failure", status_code=500
+        )
