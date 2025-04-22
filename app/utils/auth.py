2a85cf7a37780a9f9b611c6e5074d27af0aedeea
hak2881 <gkr054@naver.com>
2025-04-22 13:29:01 +0900

    ✨Ref: comment 리팩토링

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -2,7 +2,7 @@ from app.utils.exception import CustomException
 
 
 async def check_author(obj, current_user):
-    """지원자 작성자인지 검증"""
+    """작성자인지 검증"""
     if obj.user_id != current_user.id and not current_user.is_superuser:
         raise CustomException(
             error="해당 작업을 수행할 권한이 없습니다.",
