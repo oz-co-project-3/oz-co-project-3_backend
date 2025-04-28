cad274d462be205c074bb36ac93f2cd9fe46403d
Ibiella <rubyhae@gmail.com>
2025-04-29 04:28:15 +0900

    :bug: 제목 검증 exception 추가

--- app/exceptions/job_posting_exceptions.py
+++ app/exceptions/job_posting_exceptions.py
@@ -13,3 +13,10 @@ class NotificationNotFoundException(CustomException):
         super().__init__(
             status_code=404, code="notification_not_found", error="등록된 공고를 찾을 수 없습니다."
         )
+
+
+class SameTitleExistException(CustomException):
+    def __init__(self):
+        super().__init__(
+            status_code=400, code="same_title_exist", error="같은 제목의 공고가 존재합니다."
+        )
